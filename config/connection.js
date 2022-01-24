4b9194df4f37df3c53c689eeb784b344071f4482
Anseera <anseerashraf1@gmail.com>
2022-01-24 23:55:09 +0530

    updation

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const mongoClient=require("mongodb").MongoClient
 const state={db:null}
 
 module.exports.connect=function(done){
-    const url='mongodb://localhost:27017'
+    const url='mongodb+srv://anseera:8mJRVkZN6mhN4gnN@cluster0.x74g0.mongodb.net/test'
     const dbname = 'texol'
     mongoClient.connect(url,{useUnifiedTopology:true},(err,data)=>{
         if(err) return done(err)
